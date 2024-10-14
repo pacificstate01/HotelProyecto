99c24b5e8a2865789768598165b307c000c45c1a
pacificstate01 <javier.otarola@inacapmail.cl>
2024-10-14 01:28:43 -0300

    sweetalert fix

--- HotelAdmin/Hotel/static/js/login.js
+++ HotelAdmin/Hotel/static/js/login.js
@@ -1,49 +1,49 @@
-//Array con datos de usuario para login, actua como una "base de datos"
-const users = [
-    {username: "andrecollao@gmail.com", password: "12345678"},
-    {username: "benjazu@gmail.com", password: "987654321"},
-    {username: "javierota@gmail.com", password: "01010101"}
-];
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("login.js is loaded");
+   //Array con datos de usuario para login, actua como una "base de datos"
+    const users = [
+        {username: "andrecollao@gmail.com", password: "12345678"},
+        {username: "benjazu@gmail.com", password: "987654321"},
+        {username: "javierota@gmail.com", password: "01010101"}
+    ];
 
-//Valida si los datos ingresados estand entro del array
-function credenciales(username,password){
-    for(const user of users){
-        if(user.username === username && user.password === password){
-            return true;
+    //Valida si los datos ingresados estand entro del array
+    function credenciales(username,password){
+        for(const user of users){
+            if(user.username === username && user.password === password){
+                return true;
+            }
         }
-    }
-    return false;
-}
-
-
-//En caso de que todo este correcto, al apretar el boton ingresar redirecciona a la pagina principal
-document.querySelector('#boton').addEventListener('click', function(event) {
-    event.preventDefault();
-    const username = document.getElementById("correo").value;
-    const password = document.getElementById("contraseña").value;
-
-    if(credenciales(username,password)){
-        //Se guarda el usuario que ingreso en localStorage
-        localStorage.setItem('loggedInUser', username);
-        Swal.fire({
-            title: 'Datos correctos',
-            text: 'Datos ingresados correctamente',
-            icon: 'success',
-            timer: 1500,
-            showConfirmButton: false
-        }).then(() => {
-            window.location.replace("/Hotel/menu/"); 
-        });
-    }
-    else{
-        Swal.fire({
-            title: 'Datos incorrectos',
-            text: 'Las credenciales ingresadas son incorrectas',
-            icon: 'error',
-            timer: 1500,
-            showConfirmButton: True
-        })
-        return;
+        return false;
     }
 
+
+    //En caso de que todo este correcto, al apretar el boton ingresar redirecciona a la pagina principal
+    document.querySelector('#boton').addEventListener('click', function(event) {
+        event.preventDefault();
+        const username = document.getElementById("correo").value;
+        const password = document.getElementById("contraseña").value;
+
+        if(credenciales(username,password)){
+            //Se guarda el usuario que ingreso en localStorage
+            localStorage.setItem('loggedInUser', username);
+            Swal.fire({
+                title: 'Datos correctos',
+                text: 'Datos ingresados correctamente',
+                icon: 'success',
+                timer: 1500,
+                showConfirmButton: false
+            }).then(() => {
+                window.location.replace("/Hotel/menu/"); 
+            });
+        }
+        else{
+            Swal.fire({
+                title: 'Datos incorrectos',
+                text: 'Las credenciales ingresadas son incorrectas',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            })
+        }
+    });
 });
